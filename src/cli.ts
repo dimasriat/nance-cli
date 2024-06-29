80c99f1f537f14d0f0ded6f18ba44b1a0207d7c7
Dimas <dimasriat@gmail.com>
2024-06-29 23:20:09 +0700

    chore: update cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -20,6 +20,19 @@ export class NanceCli {
         console.log(serverTime);
       });
 
+    program
+      .command('get-asset-price <symbol>')
+      .description('Get the current price of an asset')
+      .action(async (symbol: string) => {
+        const price = await this.nance.getCurrentAssetPrice(symbol);
+        console.log(price);
+      });
+
+    program.command('get-account-state').action(async () => {
+      const accountState = await this.nance.getAccountState();
+      console.log(accountState);
+    });
+
     return program;
   }
 }
